defd991873a3dfc32a900ca06999cbb60df1e63d
mmtrez <immtrez@gmail.com>
2022-08-11 17:58:33 +0430

    add content script

--- pageFontStyle/eventPage.js
+++ pageFontStyle/eventPage.js
@@ -1,3 +1,7 @@
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  chrome.pageAction.show(tabs[0].id);
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+  if (req.todo === "showPageAction") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.pageAction.show(tabs[0].id);
+    });
+  }
 });
