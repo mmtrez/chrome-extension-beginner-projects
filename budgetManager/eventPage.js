b647321092af1b9c1380b3ac4bfe7625eb341dc6
mmtrez <immtrez@gmail.com>
2022-08-11 14:42:24 +0430

    budget manager - context menu functionality

--- budgetManager/eventPage.js
+++ budgetManager/eventPage.js
@@ -1,5 +1,35 @@
 chrome.contextMenus.create({
-  id: "context",
+  id: "budgetManager",
   title: "Spend Money",
   contexts: ["selection"],
+});
+
+chrome.contextMenus.onClicked.addListener((e) => {
+  if (e.menuItemId === "budgetManager") {
+    if (!isNaN(e.selectionText) && e.selectionText) {
+      let totalSpent;
+      let limit;
+      chrome.storage.sync.get(["total", "limit"], (res) => {
+        totalSpent = res.total || 0;
+        limit = res.limit || 1000;
+        totalSpent += Number(e.selectionText);
+        chrome.storage.sync.set({ total: totalSpent }, () => {
+          chrome.notifications.create("added", {
+            type: "basic",
+            title: "Spent successfully.",
+            message: "added to your total spent successfully.",
+            iconUrl: "icon48.png",
+          });
+          if (totalSpent >= limit) {
+            chrome.notifications.create("limit", {
+              type: "basic",
+              title: "Limit reached!",
+              message: "Uh oh! it looks like you've reached your limit!",
+              iconUrl: "icon48.png",
+            });
+          }
+        });
+      });
+    }
+  }
 });
